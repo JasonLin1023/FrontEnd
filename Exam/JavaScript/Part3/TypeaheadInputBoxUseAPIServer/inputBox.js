ba56f886ba30d9f15b7cc9544fb4565a41525632
Wenxuan He <fiona@zuizuizuikeaideHBBs-MacBook-Air.local>
2019-08-05 16:03:28 -0700

    Add new file

---
+++ Exam/JavaScript/Part3/TypeaheadInputBoxUseAPIServer/inputBox.js
@@ -0,0 +1,136 @@
+const DIRECTION = {
+    UP: 'up',
+    DOWN: 'down'
+} 
+
+
+function view(container, model){
+    let _input = document.createElement('input'),
+        _options = document.createElement('div');
+    
+    _input.setAttribute('type', 'text');
+    _options.setAttribute('class', 'options');
+    
+    container.appendChild(_input);
+    container.appendChild(_options);
+    
+    let cb = debouce(model.fetchData, 100);
+    
+    _input.addEventListener('keyup', function(e){
+        let inputText = e.target.value;
+
+        cb(inputText);
+    });
+    
+    container.addEventListener('keyup', function(e) {
+       let keyCode = e.keyCode;
+       
+         
+       if(keyCode === 38) {
+            // UP
+            model.arrowKey(DIRECTION.UP);
+       } else if(keyCode === 40) {
+            // DOWN
+            model.arrowKey(DIRECTION.DOWN);
+       }
+    });
+    
+    
+    function render(data, selected){
+        if(!data || !data.length) {
+            _options.style.display = 'none';
+        } else {
+            _options.innerHTML = '';
+            
+            for(let i=0;i<data.length;i++) {
+                let item = data[i];
+
+                let singleOption = document.createElement('div');
+                singleOption.innerHTML = item;
+                
+                if(i === selected) {
+                    singleOption.innerHTML = item + ' selected';
+                }
+                
+                _options.appendChild(singleOption);
+            }
+            
+            _options.style.display = 'block';
+        }
+    }
+    
+    model.subscribe(render);
+}
+
+
+
+function model(){
+    let _subscriber, _cache={}, _data, _currentFocus = -1;
+    
+    function _fetchData(text){
+        
+        if(_cache[text]) {
+            apiBack(_cache[text]);
+        } else {
+            fetch('https://swapi.co/api/people/?search=' + text)
+                .then(response => response.json())
+                .then(function(json){
+                    _cache[text] = json;
+                    apiBack(json);
+                });
+        }
+    }
+    
+    function apiBack(json) {
+        let names = json.results.map(function(item){
+            return item.name;
+        });
+        
+        _data = names;
+        
+        _subscriber(_data, _currentFocus);
+    }
+    
+    function _arrowKey(direction){
+        // update _currentFocus
+        
+        if(direction === DIRECTION.DOWN) {
+            _currentFocus++;
+            
+            _currentFocus = _currentFocus > _data.length-1 ?  _data.length-1 : _currentFocus;
+        } else if (direction === DIRECTION.UP) {
+            _currentFocus--;
+            
+            _currentFocus = _currentFocus < -1 ? -1 : _currentFocus;
+        }
+        
+        _subscriber(_data, _currentFocus);
+    }
+
+    return {
+        subscribe: function(fn) {
+            if(!_subscriber) _subscriber = fn;
+        },
+        fetchData: _fetchData,
+        arrowKey: _arrowKey
+    }
+}
+
+
+function debouce(fn, wait) {
+    let _timer;
+
+    return function(...args){
+        clearTimeout(_timer);
+
+        _timer = setTimeout(function(){
+            fn.apply(null, args);
+        }, wait);
+    };
+}
+
+
+let typeAheadContainer = document.querySelector('.typeahead-container');
+let typeAheadModel = model();
+
+let typeAheadView = view(typeAheadContainer, typeAheadModel);
